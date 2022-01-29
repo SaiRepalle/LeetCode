5c1b3a536fe876262092b9b32169edca80579e4d
SAI REPALLE <nagasai.sql@gmail.com>
2022-01-29 12:37:59 -0800

    Adding 876_linked_list structure to the repo

---
+++ LinkedLists/876_Middle_of_the_Linked_List.py
@@ -0,0 +1,18 @@
+# Middle of the Linked List
+# Given the head of a singly linked list, return the middle node of the linked list.
+# If there are two middle nodes, return the second middle node.
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head):
+        pass
+
+
+if __name__ == "__main___":
+    pass
